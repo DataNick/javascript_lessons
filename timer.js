84320f8248dc7e297f2989bad76ca38e1fb57844
DataNick <DataNick@users.noreply.github.com>
2015-11-27 17:19:03 -0500

    Added folders to directory structure and exploring prompts, confirms and alerts

--- timer.js
+++ timer.js
@@ -3,4 +3,8 @@ while (number >= 1) {
   console.log(number);
   number--;
 }
-console.log("Blast off!")
+console.log("Blast off!");
+alert("Checking alert function");
+var user = prompt("What is your name?")
+confirm("Want to go " + user + "?");
+console.log(user)
