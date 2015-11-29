040ede8f1f892152de0f0e207bb8e51b4d8e331f
DataNick <DataNick@users.noreply.github.com>
2015-11-29 15:39:52 -0500

    Added closure exercises

--- resources/js/anonymous.js
+++ resources/js/anonymous.js
@@ -11,3 +11,33 @@ ninja.shout();
 setTimeout(
   function(){ assert(true, 'Forever!'); },
   500);
+
+
+// Closures bind values at the very last moment
+
+function assignTorpedo(name, passengerArray) {
+  var torpedoAssignment;
+  for (var i = 0; i < passengerArray; i++) {
+    if (passengerArray[i] == name) {
+      torpedoAssignment = function(){
+        alert("Ahoy, " + name + "!\n" +
+          "Man your post at Torpedo #" + (i + 1) + "!");
+      };
+    }
+    return torpedoAssignment;
+  }
+}
+// This will run the loop for ALL the items in array
+function assignTorpedo(name, passengerArray) {
+  for (var i = 0; i < passengerArray; i++) {
+    if (passengerArray[i] == name) {
+      return function(){
+        alert("Ahoy, " + name + "!\n" +
+          "Man your post at Torpedo #" + (i + 1) + "!");
+      };
+    }
+  }
+}
+
+// This will return function at first instance of true
+
