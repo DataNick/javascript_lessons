d7612395598544a7ee55aae5440e9a5e8c06df76
DataNick <DataNick@users.noreply.github.com>
2015-11-30 16:03:40 -0500

    Added code samples that explore managing objects within objects

--- resources/js/anonymous.js
+++ resources/js/anonymous.js
@@ -81,11 +81,84 @@ function addBook(box, name, writer){
   box["book" + box["# of Books"]] = {title: name, author: writer};
 }
 
+function add(x) {
+  return x + y;
+};
 
+function outer_add(y) {
+  return add(x);
+}
 
+// this will not work. Variable not created in scope of enclosure. Won't have access to it.
 
+function say667() {
+  // Local variable that ends up within closure
+  var num = 666;
+  var sayAlert = function() { alert(num); }
+  num++;
+  return sayAlert;
+}
 
+var sayNumber = say667();
+sayNumber(); // alerts 667
 
+function setupSomeGlobals() {
+    // Local variable that ends up within closure
+    var num = 666;
+    // Store some references to functions as global variables
+    gAlertNumber = function() { alert(num); }
+    gIncreaseNumber = function() { num++; }
+    gSetNumber = function(x) { num = x; }
+}
+
+setupSomeGlobals();
+gIncreaseNumber();
+gAlertNumber(); // 667
+gSetNumber(5);
+gAlertNumber(); // 5
+
+var oldAlert = gAlertNumber;
+
+setupSomeGlobals();
+gAlertNumber(); // 666
+
+oldAlert() // 5
+
+The three functions have shared access to the same closure — the local variables of setupSomeGlobals() when the three functions were defined.
+Note that in the above example, if you call setupSomeGlobals() again, then a new closure (stack-frame!) is created.
+
+var vehicle3 = {
+  type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
+  ranger1: {name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
+  ranger2: {name: "Bijan Boustani", skillz: "Working", dayOff: "Saturday"},
+  ranger3: {name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
+  ranger4: {name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
+  numRangers: 4
+};
+
+var relieveDuty = function (vehicle, day){
+  var offDuty = [];
+  var onDuty = [];
+
+  for (var i = 1; i <= vehicle.numRangers; i++) {
+    if (vehicle["ranger" + i].dayOff == day) {
+      offDuty.push(vehicle["ranger" + i]);
+    } else {
+      onDuty.push(vehicle["ranger" + i]);
+  }
+    delete vehicle["ranger" + i];
+  }
+
+  vehicle.numRangers -= offDuty.length;
+
+  for (var j = 1; j <= vehicle.numRangers; j++) {
+    vehicle["ranger" + j] = onDuty.shift();
+  }
+
+  return offDuty;
+};
+
+relieveDuty(vehicle3, "Friday");
 
 
 
